b11164c64ad707602422fd74330a79cea349315c
Andy Parinas <andy.parinas@gmail.com>
2019-08-03 11:26:37 +1000

    fractor index and views

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -7,39 +7,7 @@
                 </div>
                 <hr>
                 @foreach ($answers as $answer)
-                    <div class="media">
-                        @include('shared._vote', [
-                            'model' => $answer
-                        ])
-                        <div class="media-body">
-                            {!! $answer->body_html !!}
-                            <div class="row">
-                                <div class="col-md-4">
-                                    <div class="ml-auto">
-                                        @can('update', $answer)
-                                            <a href="{{ route('questions.answers.edit',[$question->id, $answer->id])}}" 
-                                                class="btn btn-sm btn-outline-info">Edit</a>                                            
-                                        @endcan
-
-                                        @can('delete', $answer)
-                                            <form class="form-delete" action="{{ route('questions.answers.destroy', [$question->id, $answer->id])}}" method="POST">
-                                                @method('DELETE')
-                                                @csrf
-                                                <button type="submit" class="btn btn-outline-danger btn-sm"
-                                                    onclick="return confirm('Are Your Sure?')">Delete</button>
-                                            </form>                                           
-                                        @endcan
-                                    </div>
-                                </div>
-                                <div class="col-md-4">
-                                    {{-- Future Content --}}
-                                </div>
-                                <div class="col-md-4">
-                                    @include('shared._author', ['model' => $answer, 'label' => 'answered'])
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    @include('answers._answer', ['answer' => $answer])
                     <hr>
                 @endforeach
             </div>
